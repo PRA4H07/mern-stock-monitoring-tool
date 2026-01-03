063dabb30dba3d1d8f9a3b9240a4142a0f482a46
Prabhdeep Singh <pra4h07@Prabhdeeps-MacBook-Air.local>
2026-01-03 23:49:25 +0530

    fix(dashboard): remove redirects from index.js to stop reload loop

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,26 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Home from "./components/Home";
 
-// 🔐 GET TOKEN FROM URL (FIRST LOGIN)
-const params = new URLSearchParams(window.location.search);
-const tokenFromUrl = params.get("token");
-
-if (tokenFromUrl) {
-  localStorage.setItem("token", tokenFromUrl);
-  window.history.replaceState({}, document.title, "/");
-}
-
-// 🔐 CHECK TOKEN IN DASHBOARD STORAGE
-const token = localStorage.getItem("token");
-
-if (!token) {
-  window.location.href = "http://localhost:3001/login";
-}
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -30,3 +15,4 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
